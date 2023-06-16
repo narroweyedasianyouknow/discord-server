b70a142a84697a5653e39e8f386381274b4ce1aa
altaev <altaev@clout.one>
2023-06-16 03:03:15 +0300

    fix separate  uploaded files

--- src/multer.ts
+++ src/multer.ts
@@ -1,14 +1,6 @@
-import { extname } from 'path';
-import { diskStorage } from 'multer';
-import { v4 as uuidv4 } from 'uuid';
+import { memoryStorage } from 'multer';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 
 export const multerOptions: MulterOptions = {
-  storage: diskStorage({
-    destination: './static/uploads',
-    filename: (req, file, callback) => {
-      const randomName = uuidv4();
-      return callback(null, `${randomName}${extname(file.originalname)}`);
-    },
-  }),
+  storage: memoryStorage(),
 };
